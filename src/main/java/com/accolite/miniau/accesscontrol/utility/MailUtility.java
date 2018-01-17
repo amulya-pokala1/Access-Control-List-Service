1fea5912aca79fe5dd469c24e808773cfc3e1c82
tarun1795 <tarun.k@accolite.com>
2018-01-17 15:14:10 +0530

    merged and fixed conflicts

--- src/main/java/com/accolite/miniau/accesscontrol/utility/MailUtility.java
+++ src/main/java/com/accolite/miniau/accesscontrol/utility/MailUtility.java
@@ -1,7 +1,5 @@
 package com.accolite.miniau.accesscontrol.utility;
 
-import javax.security.auth.Subject;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
